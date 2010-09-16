f588c8eefa1cc75d09d3a5b08a184d72437dd46f
tschmorleiz <tschmorleiz@uni-koblenz.de>
2010-09-16 20:57:22 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@41 80f8bc9e-4d75-439a-9954-e8dd026db917

--- code/hibernate2/org/softlang/HibernateConnectivity.java
+++ code/hibernate2/org/softlang/HibernateConnectivity.java
@@ -3,21 +3,29 @@ package org.softlang;
 import java.util.List;
 import org.hibernate.Session;
 import org.softlang.om.*;
+import org.softlang.util.HibernateUtil;
 
 public class HibernateConnectivity {
 
 	private Session session;
 
-	public HibernateConnectivity(Session session) {
-		this.session = session;
-		this.session.beginTransaction();
+	public HibernateConnectivity() {
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
 	}
 
 	public Company loadCompany() {
+		if (!session.isOpen())
+			session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
 		Company company = new Company();
 		// get all top departments
-		List<?> depts = session.createQuery(
-				"from Dept where UPPER_DEPT_ID is null").list();
+		List<?> depts = session.createQuery("from Dept").list();
+		List<?> subunits = session.createQuery("from Subunit").list();
+		for (Object o : subunits) {
+			Subunit subunit = (Subunit) o;
+			if (subunit.getDu() != null)
+				depts.remove(subunit.getDu());
+		}
 		// add them to the company's top department list
 		for (Object o : depts)
 			company.getDepts().add((Dept) o);
@@ -30,6 +38,7 @@ public class HibernateConnectivity {
 			session.save(dept);
 		}
 		session.getTransaction().commit();
+
 	}
 
 }
