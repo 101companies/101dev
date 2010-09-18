3640b91cac6030e01f0ca142f5fb8cf9a26be32f
rlaemmel <rlaemmel@gmail.com>
2010-09-18 23:05:42 +0000

    Initial commit
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@67 80f8bc9e-4d75-439a-9954-e8dd026db917

---
+++ code/dom/org/softlang/dom/Tests.java
@@ -0,0 +1,24 @@
+package org.softlang.dom;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import static org.junit.Assert.*;
+
+public class Tests {
+		
+    @Test
+	public void testTotal() throws Exception {
+    	Document doc = DOMUtilities.loadDocument("Year2008.xml");
+    	double total = Total.aggregate(doc);
+    	assertEquals(399747, total, 0);
+ 	}
+	
+    @Test
+    public void testCut() throws Exception {
+    	Document doc = DOMUtilities.loadDocument("Year2008.xml");
+    	Cut.transform(doc);
+    	double total = Total.aggregate(doc);
+    	assertEquals(199873.5, total, 0);
+    }    
+}
