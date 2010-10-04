2fd471b56d320bbc71cb046e5f153224d57d8e49
rlaemmel <rlaemmel@gmail.com>
2010-10-04 21:00:50 +0000

    Minor name mangling.
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@136 80f8bc9e-4d75-439a-9954-e8dd026db917

--- code/sax/org/softlang/sax/Tests.java
+++ code/sax/org/softlang/sax/Tests.java
@@ -8,28 +8,28 @@ public class Tests {
     @Test
 	public void testTrace() throws Exception {
 		Trace handler = new Trace();
-		SAXUtilities.parse(handler, "Year2008.xml");
+		SAXUtilities.parse(handler, "sampleCompany.xml");
 	}
 		
     @Test
 	public void testTotal() throws Exception {
 		Total handler = new Total();
-		SAXUtilities.parse(handler, "Year2008.xml");
+		SAXUtilities.parse(handler, "sampleCompany.xml");
 	    assertEquals(399747, handler.getTotal(), 0);		
 	}
 	
     @Test
     public void testCut() throws Exception {
-    	Cut handler1 = new Cut("Year2009.xml");
-    	SAXUtilities.parse(handler1, "Year2008.xml");
+    	Cut handler1 = new Cut("output.xml");
+    	SAXUtilities.parse(handler1, "sampleCompany.xml");
 		Total handler2 = new Total();
-		SAXUtilities.parse(handler2, "Year2009.xml");
+		SAXUtilities.parse(handler2, "output.xml");
 	    assertEquals(199873.5, handler2.getTotal(), 0);		
     }    
     
     @Test
     public void testValidate() {
-     	assertTrue(Validator.validate("Year2008.xml", "Company.xsd"));
-     	assertTrue(Validator.validate("Year2009.xml", "Company.xsd")); 	
+     	assertTrue(Validator.validate("sampleCompany.xml", "Company.xsd"));
+     	assertTrue(Validator.validate("output.xml", "Company.xsd")); 	
     }    
 }
