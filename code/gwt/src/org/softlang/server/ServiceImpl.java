228fd3367be2e21c3ac84e96e2aa49ef832cea10
tschmorleiz <tschmorleiz@uni-koblenz.de>
2010-10-06 23:37:23 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@201 80f8bc9e-4d75-439a-9954-e8dd026db917

--- code/gwt/src/org/softlang/server/ServiceImpl.java
+++ code/gwt/src/org/softlang/server/ServiceImpl.java
@@ -40,63 +40,64 @@ public class ServiceImpl extends RemoteServiceServlet implements Service {
 		joe.setName("Joe");
 		joe.setAddress("Wifi City");
 
-		Employee eCraig = new Employee();
-		Employee eErik = new Employee();
-		Employee eRalf = new Employee();
-		Employee eRay = new Employee();
-		Employee eKlaus = new Employee();
-		Employee eKarl = new Employee();
-		Employee eJoe = new Employee();
+		Employee CraigE = new Employee();
+		Employee erikE = new Employee();
+		Employee ralfE = new Employee();
+		Employee rayE = new Employee();
+		Employee klausE = new Employee();
+		Employee karlE = new Employee();
+		Employee joeE = new Employee();
 
-		eCraig.setPerson(craig);
-		eErik.setPerson(erik);
-		eRalf.setPerson(ralf);
-		eRay.setPerson(ray);
-		eKlaus.setPerson(klaus);
-		eKarl.setPerson(karl);
-		eJoe.setPerson(joe);
+		CraigE.setPerson(craig);
+		erikE.setPerson(erik);
+		ralfE.setPerson(ralf);
+		rayE.setPerson(ray);
+		klausE.setPerson(klaus);
+		karlE.setPerson(karl);
+		joeE.setPerson(joe);
 
-		eCraig.setSalary(123456);
-		eErik.setSalary(12345);
-		eRalf.setSalary(1234);
-		eRay.setSalary(234567);
-		eKlaus.setSalary(23456);
-		eKarl.setSalary(2345);
-		eJoe.setSalary(2344);
+		CraigE.setSalary(123456);
+		erikE.setSalary(12345);
+		ralfE.setSalary(1234);
+		rayE.setSalary(234567);
+		klausE.setSalary(23456);
+		karlE.setSalary(2345);
+		joeE.setSalary(2344);
 
 		Dept research = new Dept();
-		Subunit s01 = new Subunit();
-		Subunit s02 = new Subunit();
+		Subunit researchS1 = new Subunit();
+		Subunit researchS2 = new Subunit();
 
-		research.setManager(eCraig);
+		research.setManager(CraigE);
 		research.setName("Research");
-		research.getSubunits().add(s01);
-		research.getSubunits().add(s02);
-		s01.setPu(eErik);
-		s02.setPu(eRalf);
+		researchS1.setPu(erikE);
+		researchS2.setPu(ralfE);
+		research.getSubunits().add(researchS1);
+		research.getSubunits().add(researchS2);
+
 
 		Dept development = new Dept();
-		development.setManager(eRay);
+		development.setManager(rayE);
 		development.setName("Development");
-		Subunit s11 = new Subunit();
-		development.getSubunits().add(s11);
-		Dept d11 = new Dept();
-		d11.setName("Dev1");
-		d11.setManager(eKlaus);
-		s11.setDu(d11);
-		Subunit s111 = new Subunit();
-		d11.getSubunits().add(s111);
-		Dept d111 = new Dept();
-		d111.setName("Dev1.1");
-		d111.setManager(eKarl);
-		s111.setDu(d111);
-		Subunit s1111 = new Subunit();
-		s1111.setPu(eJoe);
-		d111.getSubunits().add(s1111);
+		Subunit developmentS1 = new Subunit();
+		development.getSubunits().add(developmentS1);
+		Dept dev1 = new Dept();
+		dev1.setName("Dev1");
+		dev1.setManager(klausE);
+		developmentS1.setDu(dev1);
+		Subunit dev1S1 = new Subunit();
+		dev1.getSubunits().add(dev1S1);
+		Dept dev11 = new Dept();
+		dev11.setName("Dev1.1");
+		dev11.setManager(karlE);
+		dev1S1.setDu(dev11);
+		Subunit dev11S1 = new Subunit();
+		dev11S1.setPu(joeE);
+		dev11.getSubunits().add(dev11S1);
 
 		company.getDepts().add(research);
 		company.getDepts().add(development);
-
+		
 		this.company = company;
 
 	}
