7ac549e0c7c9748abc12c8bccfa75bd4ac34abdc
avaranovich <dotnetby@gmail.com>
2011-03-06 21:07:49 +0000

    added an extension to support a new syntax "[[[ | ]]]" which automatically creates links for categories
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@388 80f8bc9e-4d75-439a-9954-e8dd026db917

--- tools/mediawiki/extensions/categorylink/categorylink.php
+++ tools/mediawiki/extensions/categorylink/categorylink.php
@@ -5,12 +5,21 @@ if (!defined('MEDIAWIKI')) die('Not an entry point.');
 $wgHooks['InternalParseBeforeLinks'][] = 'fnMyHook';
 
 function fnMyHook( &$parser, &$text ) { 
- $pattern = "/(\[)([\w\W\s][^\]]+)(\])/"; //looking for [[[text]]]
+ $pattern = "/(\[{3})([\w\W\s][^\]]+)(\]{3})/"; 
  preg_match_all($pattern, $text, $out, PREG_PATTERN_ORDER); 
-
+ 
  $i = 0;
  foreach($out[0] as $k=>$v) {
+    $c = substr_count($out[2][$i],"|"); 
+    var_dump($c);
+	
     $replacement = "[[:Category:" . $out[2][$i] . "|" . $out[2][$i] . "]]";
+		
+    if(substr_count($out[2][$i],"|") == 1){ //looking for xxx|yyy
+	list($s1, $s2) = split('|', $out[2][$i]);
+	$replacement = "[[:Category:" . $out[2][$i] . "]]";
+    }   
+    
     $i++;
     $text = str_replace($v, $replacement, $text);
  } 
