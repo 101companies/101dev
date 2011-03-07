fbca3dbd98052c240ec5e6c987732ef3e76954fe
avaranovich <dotnetby@gmail.com>
2011-03-07 14:28:36 +0000

    added an extension to support a new syntax "[[[ | ]]]" which automatically creates links for categories if they are exist and normal link if not
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@392 80f8bc9e-4d75-439a-9954-e8dd026db917

--- tools/mediawiki/extensions/categorylink/categorylink.php
+++ tools/mediawiki/extensions/categorylink/categorylink.php
@@ -10,15 +10,22 @@ function fnMyHook( &$parser, &$text ) {
    preg_match_all($pattern, $text, $out, PREG_PATTERN_ORDER); 
  
    $bot = 'Bot';
-   $wiki = '101companies';
+   $wiki = 'Wiki';
    $wpapi = new wikipediaapi ('', '', '', $bot, $wiki, true );
-   $categories = $wpapi->listcategories();
+   $categories = $wpapi->listcategories(); //listprefix('Category'); 
+   //var_dump($categories);
+   
    $categoryNames = array();
    foreach($categories as $c){
        array_push($categoryNames, $c['*']);
+       $subCats = $wpapi->categorymembers("category:" . $c['*']);
+       foreach($subCats as $sc){
+          $t = str_replace("Category:", "", $sc['title']); // take Category:Test as input and produce only Test
+          array_push($categoryNames, $t);
+      }       
    }
    var_dump($categoryNames);
-   
+    
    $i = 0;
    foreach($out[0] as $k=>$v) {
     $category = $out[2][$i];
