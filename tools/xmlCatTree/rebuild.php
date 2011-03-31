49e4dc475bf526f8a63aae70ec52144452b1d296
tschmorleiz <tschmorleiz@uni-koblenz.de>
2011-03-31 18:14:06 +0000

    Intent extraction removed from rebuild
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@633 80f8bc9e-4d75-439a-9954-e8dd026db917

--- tools/xmlCatTree/rebuild.php
+++ tools/xmlCatTree/rebuild.php
@@ -5,45 +5,19 @@
     return (strcmp(substr($text, 0, strlen($suffix)),$suffix)===0);
  }
  
- function extractIntent($wpq,$name) {
-    $content = $wpq->getpage($name);
-    $inIntent= false;
- 	$indent = "";
-    foreach(explode(PHP_EOL, $content) as $line) {
-      if($inIntent) {
-          if (startsWith("==",trim($line))) {
-            break;
-          }
-          if ($line != '') {
-          	$intent = $line;
-          	break;
-          }
-      }
-      if (startsWith("==Intent==",str_replace(' ','',trim($line)) )) {
-        $inIntent= true;
-      }  
-    }
-    return $intent;
- }
  
- function addNode(&$parent, $cat, $wpapi,$wpq) {
+ function addNode(&$parent, $cat, $wpapi) {
  	$catNode = $parent->addChild("cat");
  	$catNode->addAttribute("name", $cat);
  	$members = $wpapi->categorymembers($cat);
- 	$content = $wpq->getpage($cat);
- 	$intent = extractIntent($wpq,$cat);
-    $catNode->addAttribute("indent", $intent);
- 	echo $intent;
  	
  	foreach((array)$members as $member) {
  	 $title = $member['title'];
  	 if(startsWith("Category:",$title))
- 	 	addNode($catNode, $title,$wpapi,$wpq);
+ 	 	addNode($catNode, $title,$wpapi);
  	 else {
  	    $pageNode = $catNode->addChild("page");
  	    $pageNode->addAttribute("name", $title);
- 	    $pIntent = extractIntent($wpq,$title);
- 	    $pageNode->addAttribute("intent", $pIntent);
  	    
  	 }
  	}
@@ -57,13 +31,11 @@
  
  $xml = simplexml_load_file('catTree.xml');
  $wpapi	= new wikipediaapi ( '', '', '', "CatBot", "101companies", true );
- 
- $wpq = new wikipediaquery($wapi,$wquery,$windex,'CatBot','101companies',true);
  $baseMembers = $wpapi->categorymembers("Base");
  foreach((array)$baseMembers as $member){
     $title = $member['title'];
     echo $title;
- 	addNode($xml, $title, $wpapi,$wpq);
+ 	addNode($xml, $title, $wpapi);
  }
  
 $handle = fopen("catTree.xml", "wb"); 
