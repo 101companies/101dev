bff474a3ec83211c4b2814d5f3aa01aa7f07b253
rlaemmel <rlaemmel@gmail.com>
2011-04-11 19:09:07 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@732 80f8bc9e-4d75-439a-9954-e8dd026db917

--- tools/tagcloudgenerator/config.php
+++ tools/tagcloudgenerator/config.php
@@ -3,6 +3,6 @@ require_once(BASE_PATH . "do-not-commit/wikibot.config.php");
 require_once(BASE_PATH . "wikibotClasses/wikibot.classes.php");
 
 // Pleaase change this to the right location on your machine
-$cloudy_data_home = BASE_PATH . "cloudy/data/";
-$cloudy_home = BASE_PATH . "cloudy";
+$cloudy_data_home = BASE_PATH . "tagcloudgenerator/data/";
+$cloudy_home = BASE_PATH . "tagcloudgenerator/";
 
