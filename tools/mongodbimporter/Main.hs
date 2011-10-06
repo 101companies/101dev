8654f246f1c6e397274bffd0e5cde422e6fdf92e
tschmorleiz <tschmorleiz@uni-koblenz.de>
2011-10-06 00:00:05 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@2109 80f8bc9e-4d75-439a-9954-e8dd026db917

--- tools/mongodbimporter/Main.hs
+++ tools/mongodbimporter/Main.hs
@@ -13,19 +13,23 @@ import Types
 import Json2WP
 import WP2Doc
 import ActionRunner
-import Builder.CoverageBuilder
 
 -- pipe to localhost
 localpipe = runIOE $ connect (host "127.0.0.1")
- 
---get all file names from a dir
-getFileNames :: String -> IO [FilePath]
-getFileNames datadir = do
-	fsIO <- liftM (filterM doesFileExist) $
-	            liftM (map (\s -> datadir ++ s)) $ 
-	              getDirectoryContents datadir
-	fsIO
-
+  
+-- convert json file to doc and insert into mongoDB     
+jsonFileToMongoDB :: Pipe ->  Collection -> String -> IO ()
+jsonFileToMongoDB pipe coll path = do 
+  c <- readFile path
+  putStrLn  $ "Converting .json " ++ path ++ " to Document..."
+  let r = (wp2Doc.json2WP.unpackResult.decode) c
+  putStrLn "Done. Inserting ..."
+  run pipe (insert coll r)
+  putStrLn "Done."
+     where
+      unpackResult (Ok a) = a
+      unpackResult (Error e) = error e
+      
 -- get all JSON data from a dir 
 populatePages :: IO ()
 populatePages = do
@@ -37,27 +41,3 @@ populatePages = do
   run pipe (delete (select [] "page"))             
   fs <- fsIO              
   mapM_ (jsonFileToMongoDB pipe "page") fs
-
-populateCoverage :: IO ()
-populateCoverage = do
-  pipe <- localpipe
-  run pipe (delete (select [] "coverage"))   
-  build coverageBuilder pipe
-  return ()
-    
- 
--- convert json file to doc and insert into mongoDB     
-jsonFileToMongoDB :: Pipe ->  Collection -> String -> IO ()
-jsonFileToMongoDB pipe coll path = do 
-  c <- readFile path
-  putStrLn  $ "Converting .json " ++ path ++ " to Document..."
-  let r = (wp2Doc.json2WP.unpackResult.decode) c
-  putStrLn "Done. Inserting ..."
-  run pipe (insert coll r)
-  putStrLn "Done."
-  putStrLn ""
-  return () 
-     where
-      unpackResult (Ok a) = a
-
-      unpackResult (Error e) = error e
