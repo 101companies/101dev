6cb98602622ae059b7c0fcf693f2bb3e5b580438
andrei varanovich <dotnetby@gmail.com>
2012-01-10 22:54:05 +0100

    changes for the paper

--- tools/tagcloudgenerator/TagCloudGenerator.php
+++ tools/tagcloudgenerator/TagCloudGenerator.php
@@ -9,7 +9,7 @@ function writeFile($fileName, $arr){
   $myFile =  $GLOBALS['cloudy_data_home'] . $fileName;
  $fh = fopen($myFile, 'w') or die("can't open file");
  foreach($arr as $el){
-   $el = str_replace(" ", "_", $el);
+   $el = str_replace(" ", "", $el);
    fwrite($fh, "\"" . $el . "\"\n" );
  }
 fclose($fh);
@@ -18,18 +18,18 @@ fclose($fh);
 $allTechnologies = array();
 $allLanguages = array();
 
- $impl = getAllImplementations();
+# $impl = getAllImplementations();
  $wiki = new Wiki();
 
- if($impl == NULL) die ("implementation pages have not been downloaded, please try a bit later" . PHP_EOL);
- foreach($impl as $i){
+# if($impl == NULL) die ("implementation pages have not been downloaded, please try a bit later" . PHP_EOL);
+/* foreach($impl as $i){
   $page = getPageContent($i['title']);
   $technologies = extractTechnologies($page);
   $languages = extractLanguages($page);
   flattern($technologies, &$allTechnologies); 
   flattern($languages, &$allLanguages); 
- }
-
+ } */
+/*
  $catPage = new CategoryPage("101implementation");
  $implementationPages = $catPage->getImplementations();
  $languagePages = $wiki->getLanguagePages();
@@ -48,21 +48,25 @@ $allLanguages = array();
  writeFile("TechnologiesInImplementations.txt", $allTechnologies);
  writeFile("LanguagesInImplementations.txt", $allLanguages);
  writeFile("JavaBasedTechnologiesInImplementations.txt", $javaBasedTechnologies);
-
+*/
 $allTechnologies = array();
 $allLanguages = array();
+$allFeatures = array();
 
- $pages = getAllPages();
- if($pages == NULL) die ("pages have not been downloaded, please try a bit later" . PHP_EOL);
- 
- foreach($pages as $p){
-  $page = getPageContent($p['title']);
-  $technologies = extractTechnologies($page);
-  $languages = extractLanguages($page);
-  if($technologies != NULL) flattern($technologies, &$allTechnologies); 
-  if($languages != NULL) flattern($languages, &$allLanguages); 
- }
+ $catPage = new CategoryPage("101implementation");
+ $implementationPages = $catPage->getImplementations();
+ #$languagePages = $wiki->getLanguagePages();
 
-writeFile("TechnologiesAnywhere.txt", $allTechnologies);
-writeFile("LanguagesAnywhere.txt", $allLanguages);
+ foreach($implementationPages as $ip){
+   $technologies = $ip->getTechs();
+   $languages = $ip->getLangs();
+   $features = $ip->getFeats();
+   if($technologies != NULL) flattern($technologies, &$allTechnologies); 
+   if($languages != NULL) flattern($languages, &$allLanguages);
+   if($features != NULL) flattern($features, &$allFeatures);
+  } 
+
+writeFile("TechnologiesInImplementations.txt", $allTechnologies);
+writeFile("LanguagesInImplementations.txt", $allLanguages);
+writeFile("FeaturesPerImplementations.txt", $allFeatures);
 
