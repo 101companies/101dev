d5e6c543882e04cf4f5d20340b63c4aa88d646bd
megaplanet <megaplanet.github@megaplanet.org>
2012-04-20 23:48:07 +0200

    the php program to generate the browser is now included. It is based on megalib/YEdGraphBrowser.php

---
+++ sandbox/m12/implementations/xsdClasses/generateXsdClassArtefactViewBrowser.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../../../../../megalib/YEdGraphBrowser.php' ;
+
+define('ROOTDIR','.') ;
+define('TARGETDIR',ROOTDIR.'/docs') ;
+define('YEDGRAPHFULLNAME',ROOTDIR.'/xsdClassesArtefactView.megal.graphml') ;
+$sourceFileDirectory = ROOTDIR.'/repo' ;
+$SourceDefinitions = array(
+    array('Company.xsd','xml', array('Company'=>'4-11', 'Department'=>'13-20', 'Employee'=>'22-28')),
+    array('Company.cs','csharp', array('Company'=>'25-51', 'Department'=>'59-110', 'Employee'=>'118-155')),
+    array('ACMECorp.xml','xml'),
+    array('ACMECorpCut.xml','xml'),
+    array('CompanyXSD2CS.bat','batch'),
+    array('Demo.cs','csharp', array('CutAcmeCorp'=>'38-43')),
+    array('Operations.cs','csharp'),
+    array('Serialization.cs','csharp')
+);
+
+$generator =
+new YEdGraphBrowserGenerator(
+    YEDGRAPHFULLNAME,
+    $sourceFileDirectory,
+    $SourceDefinitions,
+    TARGETDIR) ;
