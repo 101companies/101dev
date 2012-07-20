0402d22c6be5a6b4ddf4ca39f7732c4e1ff3e098
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2012-07-20 19:47:34 +0200

    Reenabled lines highlighting

--- sandbox/m12/js/source-exploration.js
+++ sandbox/m12/js/source-exploration.js
@@ -6,6 +6,20 @@ var SourceExplorer = function($) {
 		$("#detailview .viewinfo").text(text)
 	}
 
+	var highlightSource = function(meta) {
+		$("#detailcontent pre .li1").removeClass("highlight")
+		if (meta && meta[0].lines) {
+			baseid = $("#detailcontent pre").attr("id")
+			var i = meta[0].lines.from
+			while (i <= meta[0].lines.to) {
+			$("#" + baseid + "-" + i).addClass("highlight")
+				i++
+			}
+			$("#detailcontent").animate({scrollTop: ($("#" + baseid + "-" + meta[0].lines.from).offset().top - $("#" + baseid + "-1").offset().top)}, 200)
+		}
+		
+	}
+
 	return {
 		init : function() {
 			$('#detailcontent').bind("click",(function(s){console.log(s)}))
@@ -18,41 +32,18 @@ var SourceExplorer = function($) {
 		},
 
 		showSource: function(path,meta) {
-			
-
 			setInfoText("Source view")
 			if (path != currentSource) {
 				currentSource = path
 				$("#detailcontent").
 					html("Loading source code...").
 					load("../" + path + ".geshi.html", function() {
-						if (meta && meta[0].lines) {
-							baseid = $("#detailcontent pre").attr("id")
-							var i = meta[0].lines.from
-							while (i <= meta[0].lines.to) {
-								$("#" + baseid + "-" + i).addClass("highlight")
-								i++
-							}
-						}
+						highlightSource(meta)
 					})
-			} 
+			} else {
+				highlightSource(meta)
+			}
 		},
-
-		showSourceAndHighlight : function(path, ranges) {
-			$("#detailcontent pre .li1").removeClass("highlight")
-			this.showSource(path, function() {
-				baseid = $("#detailcontent pre").attr("id")
-				alert(baseid)
-				$.each(ranges, function(i, range) {
-					var i = range.from
-					while (i <= range.to) {
-						$("#" + baseid + "-" + i).addClass("highlight")
-						i++
-					}
-					//$("#detailcontent").animate({scrollTop: ($("#" + baseid + "-" + range.from).offset().top - $("#" + baseid + "-1").offset().top)}, 200)
-				})
-			})
-		}
 	}
 }(jQuery)
 
