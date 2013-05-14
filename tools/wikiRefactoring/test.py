41e2fb380a03c1a58a947253983bfd00b04e0831
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-05-14 20:49:20 +0200

    cleaup

--- tools/wikiRefactoring/test.py
+++
@@ -1,8 +0,0 @@
-from wikitools import wiki
-import page2
-
-apiurl = "http://101companies.org/api.php"
-wiki101 = wiki.Wiki(apiurl)
-
-p = page2.Page2(wiki101, "Contribution:haskellDB")
-p.rewriteInternlink("Language:Haskelll", "Language:Haskell")
